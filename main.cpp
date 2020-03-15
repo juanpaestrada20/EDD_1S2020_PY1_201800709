afd8f14fb8d26511e95737b02b5271e9c7c91dd4
Juan Pablo Estrada <3599546500101@ingenieria.usac.edu.gt>
2020-03-14 23:34:40 -0600

    Inicio de Estructuras

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#include "ListaPunteos.h"
+#include "ArbolJugadores.h"
+#include "ColaFichas.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    setlocale(LC_ALL,"Spanish");
+    Cola *fichas = new Cola();
+
+    cout << "===========" << endl;
+    fichas->crearFichas();
+    for(int i = 0; i < 96 ; i++){
+        cout << fichas->desEnColar()->getLetra() << endl;
+    }
+
     return 0;
 }
